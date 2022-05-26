29ac6ead2fb0ed849ec38f05df9489d233e95a9e
Vinicius Farias <102328663+ViniciusAlbanit@users.noreply.github.com>
2022-05-26 09:15:44 -0300

    Create doação.py

---
+++ exercicios/doação.py
@@ -0,0 +1,12 @@
+a = float(input())
+reais=0
+vc=0
+while a != -1:
+    real = a * 2.5
+    reais +=real
+    vc +=a
+    a = float(input())
+print(f'VC$ {vc:.2f}')                                                     
+print(f'R$ {reais:.2f}')    
+
+    

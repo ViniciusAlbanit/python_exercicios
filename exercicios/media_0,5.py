2461ef61d963c97485fc10468cb81d6f95d45676
Vinicius Farias <102328663+ViniciusAlbanit@users.noreply.github.com>
2022-08-24 08:24:02 -0300

    new

---
+++ exercicios/media_0,5.py
@@ -0,0 +1,10 @@
+a = float(input())
+b = float(input())
+media = (a + b) / 2
+
+if media >= 6:
+    print(f'aprovado')
+elif a >= 2.00:
+    print(f'talvez com a sub')
+else:
+    print(f'reprovado')
---
+++ exercicios/soma_tempo.py
@@ -0,0 +1,7 @@
+a = int(input())
+b = int(input())
+c = int(input())
+
+soma = a + b + c
+
+print(f'{soma} minutos')
